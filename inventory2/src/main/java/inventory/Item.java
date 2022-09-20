d2055df6c177d801aed2d2eef749c4d3c684d021
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2022-09-20 15:43:35 +0300

    Implement refactoring procedures on Item.java

--- inventory2/src/main/java/inventory/Item.java
+++ inventory2/src/main/java/inventory/Item.java
@@ -6,16 +6,12 @@ public class Item {
 
 	private String name;
 	
-	public void setName(String name) throws Exception {
+	public void setName(String name) {
 		this.name = name;
-		if(name == null) {
-			throw new Exception("Name cannot be null");
-		}
-		if("".equals(name)) {
-			throw new Exception("Name cannot be empty");
-		}
+		nameNotNull(name);
+		nameNotEmpty(name);
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -26,5 +22,17 @@ public class Item {
 	
 	public void setAmount(BigDecimal amount) {
 		
+	}
+	
+	private void nameNotNull(String name) {
+		if(name == null) {
+			throw new RuntimeException("Name cannot be null");
+		}
+	}
+
+	private void nameNotEmpty(String name) {
+		if("".equals(name)) {
+			throw new RuntimeException("Name cannot be empty");
+		}
 	}
 }
